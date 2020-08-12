12732840f6a8ac05c0fb542626e520f292dcf850
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-12 08:20:54 +0800

    Add question

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
+    let quiz = [
+        Question(text: "1 + 1 = 2", answer: "True"),
+        Question(text: "2 + 1 = 2", answer: "True"),
+        Question(text: "3 + 1 = 2", answer: "False"),
+        Question(text: "4 + 1 = 2", answer: "True")
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         question.text = "Question?"
@@ -22,12 +31,25 @@ class ViewController: UIViewController {
 
     @IBAction func pressAnswer(_ sender: UIButton) {
         if (sender == trueButton) {
-            question.text = "Answer: True"
-            progressBar.progress = 0.25
+            //question.text = "Answer: True"
+            //progressBar.progress = 0.25
+            
         } else {
-            question.text = "Answer: False"
-            progressBar.progress = 0.75
+            //question.text = "Answer: False"
+            //progressBar.progress = 0.75
         }
+        
+        if (questionNumber+1 < quiz.count) {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        updateView()
+    }
+    
+    func updateView() {
+        question.text = quiz[questionNumber].text
     }
     
 }
